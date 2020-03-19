df07a486d853766b5a718b79c97c9f2a35496df9
HankNoodles <62364648+HankNoodles@users.noreply.github.com>
2020-03-19 17:26:25 +0800

    Use relative path of certs file

--- Testing/test.py
+++ Testing/test.py
@@ -9,7 +9,7 @@ replyOver18 = {
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"}
 session = requests.session()
-response = session.post("https://www.ptt.cc/ask/over18", verify="/home/hank/Truly-Man/certs.pem", data=replyOver18)
+response = session.post("https://www.ptt.cc/ask/over18", verify="./certs.pem", data=replyOver18)
 if response.status_code == 200:
     response = session.get("https://www.ptt.cc/bbs/Beauty/M.1584533842.A.6AB.html", headers=headers)
     if response.status_code != 200:
